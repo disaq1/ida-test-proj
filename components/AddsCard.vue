a671b30c089b98dfe6c219d0a4ac9d17dfdeea9d
Slava79198015981 <slava79198015981@gmail.com>
2021-11-15 01:20:24 +0400

    first commit

---
+++ components/AddsCard.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="add">
+    <div class="add__block point">
+      <h3>Наименование товара</h3>
+      <input type="text" placeholder="Введите наименование товара">
+    </div>
+    <div class="add__block">
+      <h3>Описание товара</h3>
+      <textarea placeholder="Введите описание товара" />
+    </div>
+    <div class="add__block point">
+      <h3>Ссылка на изображение товара</h3>
+      <input type="text" placeholder="Введите ссылку">
+    </div>
+    <div class="add__block point">
+      <h3>Цена товара</h3>
+      <input type="text" placeholder="Введите цену">
+    </div>
+    <button class="add__btn">
+      <p>Добавить товар</p>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap');
+  @mixin smooth {
+    transition: 0.3s;
+  }
+  .add {
+    width: 100%;
+    margin: 0 0 20px;
+    padding: 24px;
+
+    background: #FFFEFB;
+    box-shadow: 0 20px 30px rgba(0, 0, 0, 0.04), 0 6px 10px rgba(0, 0, 0, 0.02);
+    border-radius: 4px;
+
+    &__block {
+      margin: 0 0 16px;
+      & h3 {
+        margin: 0 0 4px;
+        font-size: 10px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 13px;
+        letter-spacing: -0.02em;
+        text-align: left;
+        color: #49485E;
+      }
+      & input,
+      & textarea {
+        width: 100%;
+        padding: 10px 16px 11px;
+        background: #FFFEFB;
+        box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+        border: none;
+        border-radius: 4px;
+        &::placeholder {
+          font-size: 12px;
+          font-style: normal;
+          font-weight: 400;
+          line-height: 15px;
+          letter-spacing: 0;
+          color: #B4B4B4;
+        }
+      }
+      & textarea {
+        height: 108px;
+        resize: none;
+      }
+    }
+    & .point {
+      & h3 {
+        position: relative;
+        display: inline;
+        &:after {
+          position: absolute;
+          top: 1px;
+          right: -5px;
+          content: '';
+          width: 4px;
+          height: 4px;
+          background: red;
+          border-radius: 50%;
+        }
+      }
+    }
+    &__btn {
+      width: 100%;
+      margin: 8px 0 0;
+      padding: 10px 0 11px;
+      background: #EEEEEE;
+      border: none;
+      border-radius: 10px;
+
+      cursor: pointer;
+      @include smooth;
+      & p {
+        margin: 0;
+        padding: 0;
+
+        font-family: 'Inter', sans-serif;
+        font-size: 12px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 15px;
+        letter-spacing: -0.02em;
+        text-align: center;
+
+        color: #B4B4B4;
+        @include smooth;
+      }
+      &:hover {
+        background: #cecece;
+      }
+      &:hover p {
+        color: #444;
+      }
+    }
+    @media screen and (min-width: 768px) {
+      margin: 0 16px 0 0;
+      min-width: 332px;
+      width: 332px;
+    }
+  }
+</style>
---
+++ components/Card.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="card">
+    <div class="card__delete-btn delete-btn" @click="deleteCard()">
+      <img src="../assets/img/deleteBtn.svg" alt="delete button" class="delete-btn__img">
+    </div>
+    <img :src="item.cardImage" :alt="item.cardName" class="card__img">
+    <h2 class="card__name">
+      {{ item.cardName }}
+    </h2>
+    <p class="card__description">
+      {{ item.cardDescription }}
+    </p>
+    <span class="card__price">
+      {{ moneyFormat(item.cardPrice)}}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+  methods: {
+    deleteCard () {
+      console.log('deleted')
+    },
+    moneyFormat (number) {
+      return parseFloat(number).toFixed(2).replace(/(\d)(?=(\d{3})+\.w)/g, '$1 ').replace(/ /g, ' ')
+    }
+
+  }
+}
+</script>
+
+<style lang="scss">
+  .card {
+    width: 100%;
+    height: 100%;
+    max-width: 550px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+
+    position: relative;
+
+    background: #FFFEFB;
+    box-shadow: 0 20px 30px rgba(0, 0, 0, 0.04), 0 6px 10px rgba(0, 0, 0, 0.02);
+    border-radius: 4px;
+
+    &:hover {
+      cursor: pointer;
+    }
+    &:hover .delete-btn {
+      display: block;
+    }
+    &__delete-btn {
+      display: none;
+      width: 32px;
+      height: 32px;
+      position: absolute;
+      top: -8px;
+      right: -8px;
+      margin: 0;
+      padding: 8px;
+      background: #FF8484;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+      border-radius: 10px;
+      cursor: pointer;
+      z-index: 2;
+    }
+    &__img {
+      width: 100%;
+      max-height: 200px;
+      height: 100%;
+    }
+    &__name {
+      margin: 16px 0 16px 16px;
+      padding: 0;
+
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 25px;
+      letter-spacing: 0;
+      text-align: left;
+    }
+    &__description {
+      margin: 0 16px 32px;
+      padding: 0;
+
+      height: 80px;
+      overflow: scroll;
+
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 20px;
+      letter-spacing: 0;
+      text-align: left;
+    }
+    &__price {
+      margin: 0 0 24px 16px;
+      padding: 0;
+
+      font-size: 24px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 30px;
+      letter-spacing: 0;
+      text-align: left;
+    }
+    @media screen and (min-width: 1204px) {
+      max-width: 332px;
+    }
+  }
+</style>
